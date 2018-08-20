72e1e8f788355ed28f8438efd061946013d2030f
Norio Suzuki <norio.suzuki@gmail.com>
2018-08-21 08:31:04 +0900

    Add ast/ast.go

---
+++ src/monkey/ast/ast.go
@@ -0,0 +1,46 @@
+package ast
+
+import "github.com/suzuki/interpreter-in-go/src/monkey/token"
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Nodes
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	ExpressionNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+type LetStatement struct {
+	Token token.Token // token.LET
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode()       {}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Line }
+
+type Identifier struct {
+	Token token.Token // token.IDENT
+	Value string
+}
+
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Line }
